b324536219e3ff3fc8d81cc80fa48eb2bc739ce8
Jason Hill <qbfreak@qbfreak.net>
2016-02-21 21:45:23 -0500

    Added client

---
+++ dean.lua
@@ -0,0 +1,14 @@
+-- dean.lua v1.0 - Dean Winchester - He's been to hell and back. Litterally.
+--	Quick and Dirty Turtle Task System - Client
+
+turtleName = "Dean"
+
+rednet.open("left")
+rednet.broadcast("HELLO Dean")
+
+id,msg = rednet.receive(5)
+if id then
+	print("Response from server!")
+else
+	print("No response from server")
+end
