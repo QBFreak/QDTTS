38f71031ad481a20b21c17eb3da071e46aaea9cf
Jason Hill <qbfreak@qbfreak.net>
2016-02-22 13:05:07 -0500

    Added multiple command support, query command to fetch turtle status from server

--- everly.lua
+++ everly.lua
@@ -4,6 +4,8 @@
 local tArgs = { ... }
 myName = "Everly"
 
+validCommands = {"shutdown", "query"}
+
 function displayHelp()
 	print("QDTTS Control Console")
 	print("Usage: "..string.lower(myName).." <command>")
@@ -11,7 +13,11 @@ function displayHelp()
 end
 
 function displayValidCommands()
-	print("Valid commands: shutdown")
+	local validCmdString = ""
+	for i,cmd in ipairs(validCommands) do
+		validCmdString = validCmdString.." "..cmd
+	end
+	print("Valid commands:"..validCmdString)
 end
 
 if table.getn(tArgs) == 0 then
@@ -19,13 +25,72 @@ if table.getn(tArgs) == 0 then
 	return
 end
 
-if tArgs[1] ~= "shutdown" then
+validCommand = false
+for i,cmd in ipairs(validCommands) do
+	if tArgs[1] == cmd then
+		validCommand = true
+	end
+end
+
+if validCommand == false then
 	print("Invalid command")
 	displayValidCommands()
 	return
 end
 
+local command = tArgs[1]
 rednet.open("top")
-rednet.broadcast("SHUTDOWN "..myName)
-print("Shutdown command sent")
 
+-- Shutdown the Turtle server
+if command == "shutdown" then
+	rednet.broadcast("SHUTDOWN "..myName, "QDTTS")
+	print("Shutdown command sent")
+end
+
+-- Query a Turtle's status
+if command == "query" then
+	queryID = tArgs[2]
+	if queryID == nil then
+		print("Usage: "..string.lower(myName).." query <number>")
+		print("  Number corresponds to the RedNet node number of the turtle you wish to query")
+	else
+		rednet.broadcast("QUERY "..myName.." "..queryID, "QDTTS")
+		local command = nil
+		local ctr = 0
+		while (command ~= "QUERYR") and (ctr < 5) do
+			ctr = ctr + 1
+			id,msg = rednet.receive("QDTTS", 2)
+
+			if msg ~= nil then
+				message = {}
+				count = 1
+				for i in string.gmatch(msg, "%S+") do
+					message[count] = i
+					count = count + 1
+				end
+				if table.getn(message) > 0 then
+					command = message[1]
+				end
+			end
+		end
+
+		if command == nil then
+			print("Request timed out")
+		else
+			if table.getn(message) < 7 then
+				print("Error: Malformed query response")
+			end
+			local server = message[2]
+			local rID = message[3]
+			local rName = message[4]
+			local rStatus = message[5]
+			local rPriority = message[6]
+			local rType = message[7]
+			print("------ " .. rName .. "------")
+			print("RedNet ID:  " .. rID)
+			print("Type:       " .. rType)
+			print("Status:     " .. rStatus)
+			print("Priority:   " .. rPriority)
+		end
+	end
+end
