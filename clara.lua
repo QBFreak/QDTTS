d319b056f4f1e245d59831c943988ec3ae71151c
Jason Hill <qbfreak@qbfreak.net>
2016-02-22 13:02:07 -0500

    Added protocol filter, storage of turtle status data, better handling of packets.

--- clara.lua
+++ clara.lua
@@ -5,41 +5,90 @@ rednet.open("top")
 
 local myName = "Clara"
 
-function addTurtle(tName)
-	if tName == nil then
-		return
+function addTurtle(tID, tName, tType)
+	if tID == nil then
+		return false
 	end
-	turtles[tables.getn(turtles)] = tName
+	local turtleData = {}
+	turtleData.name = tName
+	turtleData.status = "idle"
+	turtleData.priority = 0
+	turtleData.type = tType
+	turtles[tID] = turtleData
+	return true
 end
+
+turtles = {}
+tasks = {}
 
 print("Server '"..myName.."' started.")
 rednet.broadcast("SERVER "..myName)
 
-turtles = {}
-
 local running = true
 while running do
-	id,msg = rednet.receive()
+	id,msg = rednet.receive("QDTTS")
 	
 	message = {}
-	count = 1
+	count = 0
 	for i in string.gmatch(msg, "%S+") do
-	  message[count] = i
 	  count = count + 1
+	  message[count] = i
 	end
 	
-	command = message[1]
-	turtleName = message[2]
+	--print("RX("..count.."): "..msg)
 	
-	if command == "HELLO" then
-		print("Greeting new Turtle " .. turtleName)
-		rednet.broadcast("SERVER "..myName)
-		addTurtle(turtleName)
-	end
-	if command == "SHUTDOWN" then
-		print("Console "..turtleName.." requested a shutdown")
-		rednet.broadcast("GOODBYE "..myName)
-		running = false
+	if count > 1 then
+		command = message[1]
+		turtleName = message[2]
+		
+		--if command == nil then print("ERR: command is nil!") end
+		--if turtleName == nil then print("ERR: turtleName is nil!") end
+		
+		--print("CMD: "..turtleName..": "..command)
+
+		-- Turtle Registration
+		if command == "HELLO" then
+			local turtleType = message[3]
+			print("Greeting "..turtleName)
+			rednet.broadcast("SERVER "..myName, "QDTTS")
+			if addTurtle(id, turtleName, turtleType) then
+				print("Registered new "..turtleType..", "..turtleName..", ID "..id)
+			else
+				print("Registration error. Type: "..turtleType.." Name: "..turtleName.." ID: "..id)
+			end
+		end
+		
+		-- Turtle Status
+
+		-- Console commands --
+		-- Shutdown the server
+		if command == "SHUTDOWN" then
+			print("Console "..turtleName.." requested a shutdown")
+			rednet.broadcast("GOODBYE "..myName, "QDTTS")
+			running = false
+		end
+		-- Query a Turtle's status
+		if command == "QUERY" then
+			local queriedTurtle = message[3] + 0 -- Add zero to convert our string to a number
+			
+			print("Console "..turtleName.." requested status of turtle "..queriedTurtle..".")
+			local turtleData = turtles[queriedTurtle]
+			
+			print(type(queriedTurtle))
+			print("--- Turtles ---")
+			for i,t in pairs(turtles) do
+				print(i,t)
+			end
+			
+			local queryResponse = queriedTurtle .. " "
+			queryResponse = queryResponse .. turtleData.name .. " "
+			queryResponse = queryResponse .. turtleData.status .. " "
+			queryResponse = queryResponse .. turtleData.priority .. " "
+			queryResponse = queryResponse .. turtleData.type
+			rednet.broadcast("QUERYR "..myName.." "..queryResponse, "QDTTS")
+		end
+	else
+		print("Malformed packet received from "..id..": "..msg)
 	end
 end
 print("Server '"..myName.."' stopped.")
