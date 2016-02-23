ffe1e6a497558e8a57942ddad9c0635cbf1a3720
Jason Hill <qbfreak@qbfreak.net>
2016-02-23 14:29:13 -0500

    Updated server reponse code to verify proper response, retry on no response

--- turtle.lua
+++ turtle.lua
@@ -4,11 +4,37 @@
 myName = "Dean"
 
 rednet.open("left")
-rednet.broadcast("HELLO Dean Turtle", "QDTTS")
 
-id,msg = rednet.receive(5)
-if id then
-	print("Response from server!")
-else
-	print("No response from server")
+local tries = 0
+local success = false
+while tries < 6 and success == false do
+	print("Searching for server")
+	rednet.broadcast("HELLO Dean Turtle", "QDTTS")
+	id,msg = rednet.receive("QDTTS", 5)
+	tries = tries + 1
+	
+	message = {}
+	count = 0
+	if msg ~= nil then
+		for i in string.gmatch(msg, "%S+") do
+			count = count + 1
+			message[count] = i
+		end
+
+		if count > 1 then
+			command = message[1]
+
+			if command == "SERVER" then
+				if message[2] == nil then
+				else
+					print("Registered with server ".. message[2])
+					success = true
+				end
+			end
+		end
+	end
+end
+
+if success == false then
+	print("No response from server!")
 end
