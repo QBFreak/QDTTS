085e3c21ba38b60a701c69b1f7582963aa3e593b
Jason Hill <qbfreak@qbfreak.net>
2017-08-22 08:43:25 -0400

    Updated for modern CC peculiarities

--- dean.lua
+++ dean.lua
@@ -1,9 +1,9 @@
 -- dean.lua v1.0 - Dean Winchester - He's been to hell and back. Litterally.
 --  Quick and Dirty Turtle Task System - Client
 
-dofile("qturtle")
+dofile("qturtle.lua")
 
-local dean = qTurtle("Dean", "turtle", "left")
+local dean = qTurtle("Dean", "turtle", "right")
 dean.register()
 
 print("I'm leaving.")
