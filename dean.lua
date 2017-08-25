5e7d76a200c1cb60f8482e458bf931614e2b02a7
Jason Hill <qbfreak@qbfreak.net>
2017-08-25 11:17:25 -0400

    Clean up and documentation

--- dean.lua
+++ dean.lua
@@ -3,15 +3,31 @@
 
 local tArgs = { ... }
 
+-- Turtle name
+local myName = "Dean"
+
+-- Task name
+local taskName = tArgs[1]
+
+if taskName == nil then
+    print("You must specify a task")
+    print("Usage: "..myName.." <task file>")
+    return
+end
+
+-- Load the qTurtle object
 dofile("qturtle.lua")
 
-local dean = qTurtle("Dean", "turtle", "right")
+-- Instantiate the qTurtle object
+local dean = qTurtle(myName, "turtle", "right")
+-- Turn on debug messages on the console
 dean.debugEnabled = true
+-- Register with the server
 dean.register()
 
-local taskName = tArgs[1] or "survey"
-
--- Run the survey code
+-- Load the task object
 dofile(taskName .. ".lua")
+-- Instantiate the task object
 local st = task(dean)
+-- Run the task
 st.run()
