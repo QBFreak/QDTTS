e9f489b03281c88658217fd214e68213d63ff528
Jason Hill <qbfreak@qbfreak.net>
2017-08-25 09:14:39 -0400

    Numerous changes:
     * Turtle orientation (facing)
     * Added self.name()
     * Added self.init() to ensure GPS OK and orient turtle
     * Automatic block detection on move
     * Rewrote self.forwardClimb() so that it actually works now
     * Added self.forwardUntil(block) and self.forwardWhile(block)
     * Added self.sink() and self.rise()
     * Changed debug() to self.debug() and updated to use rednet debug
    protocol (even when "disabled")
     * Added self.saveLoc() and self.getLoc() to store and retrieve
    locations on/from server
     * Added self.navTo() and self.navToName() - DON'T WORK ATM
     * Added self.face() to turn turtle in a specific direction once
    orientation is known

--- qturtle.lua
+++ qturtle.lua
@@ -18,6 +18,12 @@ function qTurtle(initName, initType, initSide)
   local side = initSide
   local type = initType
   local serverNotifiedNoFuel = false
+  -- Which way is the turtle facing?
+  local facing = nil    -- Unknown
+  -- Block detection
+  local blkFront = nil
+  local blkUp = nil
+  local blkDown = nil
 
   if initName == "" then
       self.debug("initName is blank!")
@@ -39,6 +45,10 @@ function qTurtle(initName, initType, initSide)
   if peripheral.getType(side) ~= "modem" then
       self.debug("Side peripheral is not a modem!")
     selfValid = false
+  end
+
+  function self.name()
+      return name
   end
 
   function self.register()
@@ -82,13 +92,62 @@ function qTurtle(initName, initType, initSide)
     return
   end
 
+  function self.North()
+      return 1
+  end
+
+  function self.South()
+      return 2
+  end
+
+  function self.East()
+      return 3
+  end
+
+  function self.West()
+      return 4
+  end
+
+  -- Initialize the turtle
+  function self.init()
+      -- Check that we have a GPS fix
+      x, y, z = gps.locate(5)
+      if x == nil then
+          self.debug("Failed to get GPS position")
+          return false
+      end
+
+      -- Orient the turtle
+      if facing == nil then
+          if self.orient() == false then
+              self.debug("Failed to orient turtle")
+              return false
+          end
+      end
+
+      -- Init block detection
+      blkFront = turtle.detect()
+      blkUp = turtle.detectUp()
+      blkDown = turtle.detectDown()
+
+      return true
+  end
+
   -- Tasks to run before moving
   function self.preMove()
     self.checkFuel()
+    -- What's in the way?
+    blkFront = turtle.detect()
+    blkUp = turtle.detectUp()
+    blkDown = turtle.detectDown()
   end
 
   -- Tasks to run after moving
   function self.postMove()
+      -- What's around us now?
+      blkFront = turtle.detect()
+      blkUp = turtle.detectUp()
+      blkDown = turtle.detectDown()
     -- Mapping will go here
   end
 
@@ -126,6 +185,107 @@ function qTurtle(initName, initType, initSide)
     return ret
   end
 
+    -- Move forward, but climb up or down to stay at ground level
+    -- TODO: check for blocks ABOVE before moving up
+    function self.forwardClimb()
+        if blkFront == false then
+            -- Nothing in front of us, lets go forward
+            if self.forward() == false then
+                -- We failed to move, stuck
+                return false
+            end
+        else
+            -- Something is in the way! (in front of us)
+            while blkFront and blkUp == false do
+                if self.up() == false then
+                    -- We failed to move, stuck
+                    return false
+                end
+            end
+            while blkFront and blkDown == false do
+                if self.down() == false then
+                    -- We failed to move, stuck
+                    return false
+                end
+            end
+            if blkFront then
+                -- We've tried to go over, we've tried to go under
+                --  we're stuck.
+                return false
+            end
+            if self.forward() == false then
+                -- We failed to move, stuck
+                return false
+            end
+        end
+
+        -- self.debug("XX Time to move down")
+        while blkDown == false do
+            -- self.debug("XX Down loop")
+            if self.down() == false then
+                -- self.debug("XX Moving down failed")
+                -- We failed to move, stuck
+                break
+            end
+        end
+
+        return true
+    end
+
+    -- TODO: Add a maximum number of steps (a sort of timeout)
+    function self.forwardUntil(blockType)
+        if blockType == nil then
+            return false
+        end
+        local s, d = turtle.inspectDown()
+        while (d.name ~= blockType) do
+            self.forwardClimb()
+            s, d = turtle.inspectDown()
+        end
+        self.debug("Found "..blockType)
+        return true
+    end
+
+    -- TODO: Add a maximum number of steps (a sort of timeout)
+    function self.forwardWhile(blockType)
+        if blockType == nil then
+            return false
+        end
+        local s, d = turtle.inspectDown()
+        while (d.name == blockType) do
+            self.forwardClimb()
+            s, d = turtle.inspectDown()
+        end
+        self.debug("Found end of "..blockType.."("..d.name..")")
+        return true
+    end
+
+    function self.sink()
+        local s, d = turtle.inspectDown()
+        while s == false do
+            if self.down() == false then
+                self.debug("sink() failed to move down")
+                return false
+            end
+            s, d = turtle.inspectDown()
+        end
+        self.debug("sink() stopped at "..d.name)
+        return true
+    end
+
+    function self.rise()
+        local s, d = turtle.inspectUp()
+        while s == false do
+            if self.up() == false then
+                self.debug("rise() failed to move down")
+                return false
+            end
+            s, d = turtle.inspectUp()
+        end
+        self.debug("rise() stopped at "..d.name)
+        return true
+    end
+
   function self.back()
     self.preMove()
     local ret = turtle.back()
@@ -150,6 +310,16 @@ function qTurtle(initName, initType, initSide)
   function self.turnLeft()
     self.preMove()
     local ret = turtle.turnLeft()
+    -- Update which direction the turtle is facing
+    if facing == self.North() then
+        facing = self.West()
+    elseif facing == self.East() then
+        facing = self.North()
+    elseif facing == self.South() then
+        facing = self.East()
+    elseif facing == self.West() then
+        facing = self.South()
+    end
     self.postMove()
     return ret
   end
@@ -157,6 +327,16 @@ function qTurtle(initName, initType, initSide)
   function self.turnRight()
     self.preMove()
     local ret = turtle.turnRight()
+    -- Update which direction the turtle is facing
+    if facing == self.North() then
+        facing = self.East()
+    elseif facing == self.East() then
+        facing = self.South()
+    elseif facing == self.South() then
+        facing = self.West()
+    elseif facing == self.West() then
+        facing = self.North()
+    end
     self.postMove()
     return ret
   end
@@ -173,10 +353,183 @@ function qTurtle(initName, initType, initSide)
       end
   end
 
-  function debug(str)
+  function self.debug(str)
       if self.debugEnabled then
           print("DEBUG: " .. str)
       end
+      rednet.broadcast(str, "DEBUG")
+  end
+
+  function self.saveLoc(locName)
+      if locName == nil then
+          self.debug("saveLoc() failed, no location name specified")
+          return false
+      end
+      local x, y, z = gps.locate(5)
+      if x == nil then
+          self.debug("saveLoc() failed, no GPS fix available")
+          return false
+      else
+          rednet.broadcast("SAVELOC "..name.." "..locName.." "..x.." "..y.." "..z, "QDTTS")
+          return true
+      end
+  end
+
+  function self.getLoc(locName)
+      if locName == nil then
+          self.debug("Location failed: No name specified")
+          return nil
+      end
+      rednet.broadcast("GETLOC "..name.." "..locName, "QDTTS")
+      local id, msg, proto = rednet.receive("QDTTS", 5)
+      if id == nil then
+          self.debug("Location failed: No answer to request")
+          return nil
+      end
+      local tblTmp = {}
+      local count = 0
+      for i in string.gmatch(msg, "%S+") do
+          tblTmp[count] = i
+          count = count + 1
+      end
+      if count ~= 6 then
+          self.debug("Location failed: Wrong answer length ("..count.."/6)")
+          return nil
+      end
+      local tblLoc = {}
+      tblLoc.name = tblTmp[2]
+      tblLoc.x = tblTmp[3]
+      tblLoc.y = tblTmp[4]
+      tblLoc.z = tblTmp[5]
+      self.debug("Location success: "..tblLoc.name)
+      return tblLoc
+  end
+
+  function self.navTo(x, y, z)
+      while true do
+          if turtle.getFuelLevel() == 0 then
+              self.debug("Navigation failed, out of fuel")
+              return false
+          end
+          if facing == nil then
+              if self.orient() == false then
+                  self.debug("Navigation failed, could not orient turtle")
+                  return false
+              end
+          end
+          local tx, ty, tz = gps.locate(5)
+          if tx == nil then
+              -- Failed to get a GPS lock
+              self.debug("Navigation failed, could not get GPS location")
+              return false
+          end
+          self.debug("DEST "..x.." "..y.." "..z)
+          self.debug("CURR "..tx.." "..ty.." "..tz)
+          if x >= y and x >= z then
+              -- Step towards X
+              if tx > x then
+                  -- Step towards X==0
+                  self.face(self.West())
+              else
+                  -- Step away from X==0
+                  self.face(self.East())
+              end
+              self.forwardClimb()
+          elseif y >= x and y >= z then
+              -- Step towards Y
+              -- How to we resolve moving towards Y and moving above/below terrain?
+              self.forwardClimb()
+          else
+              -- Step towards Z
+              if tz > z then
+                  -- Step towards Z==0
+                  self.face(self.North())
+              else
+                  -- Step away from Z==0
+                  self.face(self.South())
+              end
+              self.forwardClimb()
+          end
+      end
+  end
+
+  function self.navToName(locName)
+      tblLoc = self.getLoc(locName)
+      if tblLoc == nil then
+          return false
+      else
+          return self.navTo(tblLoc.x, tblLoc.y, tblLoc.z)
+      end
+  end
+
+  function self.orient()
+      -- Attempt to determine which direction we're facing based on a single step
+      local x1, y1, z1 = gps.locate(5)
+      if x1 == nil then
+          -- Failed to get a GPS lock
+          return false
+      end
+
+      -- MOVE IN ANY X/Z DIRECTION
+      local rot = 0
+      if turtle.detect() then
+          -- We're blocked, start the dance
+          while rot < 5 and turtle.detect() do
+              self.turnRight()
+              rot = rot + 1
+          end
+          if rot == 4 then
+              -- We need to go up or down
+              self.debug("HEY, IDIOT, YOU FORGOT TO CODE THIS")
+              return false
+          else
+              -- Woo, we found a direction, lets move and then reset
+              self.forward()
+          end
+      else
+          self.forward()
+      end
+
+      local x2, y2, z2 = gps.locate(5)
+      if x2 == nil then
+          -- Failed to get a GPS lock, should never happen if x1 is OK
+          return false
+      end
+      facing = nil
+      if x2 > x1 then
+          facing = self.East()
+      elseif x1 > x2 then
+          facing = self.West()
+      elseif z2 > z1 then
+          facing = self.South()
+      elseif z1 > z2 then
+          facing = self.North()
+      end
+      if facing == nil then
+          self.debug("orient() facing: nil!")
+      end
+      -- Move back to where we started
+      self.back()
+      while rot > 0 do
+          self.turnLeft()
+          rot = rot - 1
+      end
+      return true
+  end
+
+  function self.face(direction)
+      if direction ~= self.North() and direction ~= self.South() and direction ~= self.East() and direction ~= self.West() then
+          return false
+      end
+      if facing == nil then
+          if self.orient() == false then
+              return false
+          end
+      end
+      while facing ~= direction do
+          self.turnRight()
+      end
+      return true
   end
 
   if selfValid then
